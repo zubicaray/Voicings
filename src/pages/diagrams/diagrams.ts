bc1bdb2a0a5807adb512f4822d08336f659cc96f
zubicaray <zubicaray@gmail.com>
2022-12-21 02:00:35 +0100

    correction repositionnement sur diagramme choisi

--- src/pages/diagrams/diagrams.ts
+++ src/pages/diagrams/diagrams.ts
@@ -16,51 +16,22 @@ import { ToolsProvider}   from    '../../providers/tools/tools';
  	@ViewChild(VirtualScroll) virtualScroll: VirtualScroll;
 	
 	chord:ChordModel;
+	scrollTo:any;
 	diagYs:number[]=[];
 	
  	constructor(public navCtrl: NavController,public navParams: NavParams,public loadingCtrl: LoadingController,private toolsProvider:ToolsProvider) {
  		super(loadingCtrl);
  		this.chord=navParams.get('chord');
-
-		var intervalH = 85/7;
-		var offset=12.5;
-		var cpt:number=0;
-		
-		var max:number=0;
-		var summax:number=0;
-		var T:number;
-		
-		//TODO: store this in model
-		this.chord.diagrams.forEach(  (d,idx) =>
-			{
-				T=(3+d.stretch)*intervalH+offset;
-				//console.log("d.stretch="+d.stretch);
-				max=Math.max(max,T);
-				if(cpt==4)
-				{					
-					summax+=max;
-					//console.log("max="+max);
-					this.diagYs.push( summax);					
-					max=0;
-					cpt=0;
-				}
-				cpt++;
-				
-			}
-		);
-
-		
 		
 		
  	}
 
  	ionViewDidEnter() {
  		
- 		//debugger
-	    this.content.scrollTo(0, this.chord.idDiag_Y-200, 0);
-		//trick so as to refresh:
-	    //this.content.scrollTo(0, this.chord.idDiag_Y-2, 100);
-		
+		this.scrollTo = document.getElementById("col-"+this.chord.idDiag);
+		if (this.scrollTo != null) 
+			this.scrollTo.scrollIntoView();
+			
 	
     	
  	}
@@ -69,24 +40,15 @@ import { ToolsProvider}   from    '../../providers/tools/tools';
 		this.chord.idDiag=idDiag;
 		
 		
-		/*
-		console.log("event.clientY:"+event.clientY)
-		console.log("event.pageY:"+event.pageY)
-		console.log("event.offsetY:"+event.offsetY)
-		*/
-		//console.log("event.layerY:"+event.layerY)
-		let d:number=Math.floor(idDiag/4);
-		this.chord.idDiag_Y=this.diagYs[d];
-		//console.log("this.chord.idDiag_Y:"+this.chord.idDiag_Y)
-		//debugger
-		//this.content.scrollTo(0,event.clientY, 10);
-		
+				
  		//this.content.scrollTo(0, this.chord.idDiag_Y-400, 100);
  		this.chord.canvas=this.chord.diagrams[idDiag];
  		var ptiches:string[]=this.chord.getCanvasPitches();
 		this.toolsProvider.playChord(ptiches);
 		
  	}
+
+	
 
  	toTop(){
  		this.content.scrollToTop(); 		
