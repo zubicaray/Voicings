193f80b0ef16bd38beff4fc926ffc4ef37e2c69a
zubicaray <zubicaray@gmail.com>
2022-12-22 15:25:54 +0100

    lazy loading des diagrammes

--- src/pages/diagrams/diagrams.ts
+++ src/pages/diagrams/diagrams.ts
@@ -1,14 +1,15 @@
 import { Component ,ViewChild} from '@angular/core';
+import { BehaviorSubject } from "rxjs"
 import { Content , NavController, NavParams ,LoadingController,VirtualScroll} from 'ionic-angular';
-import { ChordModel} from '../../models/chordModel';
+import { ChordModel,DiagramType} from '../../models/chordModel';
 import { LoadingCtrlPage}  from    '../loading-ctrl/loading-ctrl';
 import { ToolsProvider}   from    '../../providers/tools/tools'; 
-
+import { TranslationProvider } from '../../providers/translation/translation';
 
 
  @Component({
  	selector: 'page-diagrams',
- 	templateUrl: 'diagrams.html',
+ 	templateUrl: 'diagrams.html'
  })
  export class DiagramsPage extends LoadingCtrlPage {
 
@@ -17,20 +18,72 @@ import { ToolsProvider}   from    '../../providers/tools/tools';
 	
 	chord:ChordModel;
 	scrollTo:any;
-	diagYs:number[]=[];
-	
- 	constructor(public navCtrl: NavController,public navParams: NavParams,public loadingCtrl: LoadingController,private toolsProvider:ToolsProvider) {
+
+	takeFirst:number=1000;
+	upAThousand:boolean=false;
+	cptTaken:number=0;
+	loading:any;
+
+	Diagrams: BehaviorSubject<DiagramType[]>;
+ 	constructor(public navCtrl: NavController,private TP: TranslationProvider,public navParams: NavParams,
+		public loadingCtrl: LoadingController,private toolsProvider:ToolsProvider) {
  		super(loadingCtrl);
  		this.chord=navParams.get('chord');
+
+		 this.cptTaken=this.takeFirst;
+		if(this.chord.diagrams.length>this.takeFirst){
+			this.upAThousand=true;
+			
+		}
+		this.Diagrams=new BehaviorSubject<DiagramType[]>([]);
+
+		this.Diagrams.next(this.chord.diagrams);
+
 		
+
 		
  	}
+	 
+	add(){
+
+		this.loading = this.loadingCtrl.create({
+			spinner: 'bubbles',
+			content: this.TP.tr('Adding chords ...'),
+			cssClass: "my-loading ",
+			duration: 3000
+		});
+		this.loading.present().then(()=>{ 
+		
+			this.cptTaken+=this.takeFirst;
+			
+			
+		},
+		);
+
+		
+		
+	
+		
+	}
 
  	ionViewDidEnter() {
- 		
+
+		//this.Diagrams.next(this.chord.diagrams.slice(this.takeFirst));
+
+		/*
+		this.Diagrams.pipe(
+			merge(Observable.of(this.chord.diagrams)))
+		;
+		this.Diagrams.subscribe(t=>
+			{console.log('ici 2')});
+
+		
+		
 		this.scrollTo = document.getElementById("col-"+this.chord.idDiag);
-		if (this.scrollTo != null) 
+		if (this.chord.idDiag < this.takeFirst && this.scrollTo != null) 
 			this.scrollTo.scrollIntoView();
+		
+		*/
 			
 	
     	
